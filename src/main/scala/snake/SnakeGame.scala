c6ad01268dab9872b880bcf6dee6b3d162d6ccbd
DennisVNilsson <dennis.vilhelm@outlook.com>
2020-11-17 10:15:35 +0100

    Initial commit

---
+++ src/main/scala/snake/SnakeGame.scala
@@ -0,0 +1,77 @@
+package snake
+import introprog.BlockGame
+
+abstract class SnakeGame(title: String) extends BlockGame(
+  title, dim = (50, 30), blockSize = 15, background = Colors.Background,
+  framesPerSecond = 50, messageAreaHeight = 3
+) {
+  var entities: Vector[Entity] = Vector.empty
+
+  var players: Vector[Player] = Vector.empty
+
+  sealed trait State
+  case object Starting extends State
+  case object Playing  extends State
+  case object GameOver extends State
+  case object Quitting extends State
+
+  var state: State = Starting
+
+  def enterStartingState(): Unit = {clearMessageArea();
+    drawTextInMessageArea(msg = "Tryck space för start", 200, 100, Colors.Background, blockSize)
+    } // rensa, meddela "tryck space för start"
+
+  def enterPlayingState(): Unit = {
+    entities.foreach(_.erase);
+    entities.foreach(_.draw)
+    state = Playing }// rensa, rita alla entiteter
+
+  def enterGameOverState(): Unit = {pixelWindow.drawText("GAME OVER", 200, 100, size = 16)
+    state = GameOver} // meddela "game over"
+
+  def enterQuittingState(): Unit = {
+    println("Goodbye!")
+    pixelWindow.hide()
+    state = Quitting
+  }
+
+  def randomFreePos(): Pos = {      ???
+    // import scala.util.Random
+    // val rnd = new Random
+
+  } // dra slump-pos tills ledig plats, används av frukt
+
+  override def onKeyDown(key: String): Unit = {
+    println(s"""key "$key" pressed""")
+    state match {
+      case Starting => if (key == " ") enterPlayingState()
+      case Playing => players.foreach(_.handleKey(key))
+      case GameOver =>
+        if (key == " ") enterPlayingState()
+        else if(key == "Escape") enterQuittingState()
+      case _ =>
+    }
+  }
+
+  override def onClose(): Unit = enterQuittingState()
+
+  def isGameOver(): Boolean  // abstrakt metod, implementeras i subklass
+
+  override def gameLoopAction(): Unit = {
+    if (state == Playing) {
+      entities.foreach(_.erase)
+      entities.foreach(_.update)
+      entities.foreach(_.draw)
+      if (isGameOver) enterGameOverState()
+    }
+  }
+
+
+  def startGameLoop(): Unit = {
+    pixelWindow.show()  // möjliggör omstart även om fönstret stängts...
+    enterStartingState()
+    gameLoop(stopWhen = state == Quitting)
+  }
+
+  def play(playerNames: String*): Unit // abstrakt, implementeras i subklass
+}
